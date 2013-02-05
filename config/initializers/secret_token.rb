b179a0676c81ad7e0d8ca08d3d3fbc86403208d2
Christopher and Kriti <fake@email.com>
2013-02-05 09:31:28 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BadJokesApp::Application.config.secret_token = 'b9c52f153a0aa3c878f92495358afd51c091d1c66c5bd3a930386dee2e81c1d80cc5043847efb5b19ce9358f5088b74e6162e7dc8756df70f1112399859c86a9'
